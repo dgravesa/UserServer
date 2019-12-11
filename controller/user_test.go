2b8f9ed2349c694cb542ffc5d2c5d0aedd2042f1
Daniel Graves <dgravesa@gmail.com>
2019-12-10 22:11:09 -0600

    adding some initial tests; implementing unmarshal for user struct

--- controller/user_test.go
+++ controller/user_test.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
-	"os"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/dgravesa/WaterLogger-UserServer/data"
@@ -10,10 +13,10 @@ import (
 
 var testUserData = []model.User{
 	model.User{Name: "Jeremy1"},
-	model.User{Name: "WaterDrinkerGuy"},
+	model.User{Name: "WaterDrinkerGuy1000"},
 }
 
-func TestMain(m *testing.M) {
+func initTestUserData(testData []model.User) {
 	userData := data.NewInMemoryUserStore()
 
 	for _, user := range testUserData {
@@ -21,51 +24,92 @@ func TestMain(m *testing.M) {
 	}
 
 	model.SetUserDataLayer(userData)
-
-	os.Exit(m.Run())
 }
 
 func Test_GetUserByName_WhenNameExists_ReturnsCorrectUser(t *testing.T) {
+	// Arrange
+	expectedCode := http.StatusOK
+	userName := "WaterDrinkerGuy1000"
+	target := fmt.Sprintf("http://localhost/user?name=%s", userName)
+	req := httptest.NewRequest("GET", target, nil)
+	res := httptest.NewRecorder()
 
+	// Act
+	userHandler(res, req)
+
+	// Assert
+	if res.Code != expectedCode {
+		t.Errorf("expected status code = %d, received status code = %d", expectedCode, res.Code)
+	} else {
+		var user model.User
+		if err := json.Unmarshal(res.Body.Bytes(), &user); err != nil {
+			t.Errorf("error decoding user: %s", err)
+		} else if userName != user.Name {
+			t.Errorf("expected user name = %s, received user name = %s", userName, user.Name)
+		}
+	}
 }
 
 func Test_GetUserByName_WhenNameDoesNotExist_ReturnsNotFound(t *testing.T) {
+	// Arrange
+	expectedCode := http.StatusNotFound
+	userName := "NotARealUser"
+	target := fmt.Sprintf("http://localhost/user?name=%s", userName)
+	req := httptest.NewRequest("GET", target, nil)
+	res := httptest.NewRecorder()
 
+	// Act
+	userHandler(res, req)
+
+	// Assert
+	if res.Code != expectedCode {
+		t.Errorf("expected status code = %d, received status code = %d", expectedCode, res.Code)
+	}
 }
 
 func Test_GetUserByID_WhenIDExists_ReturnsCorrectUser(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
 
 func Test_GetUserByID_WhenIDDoesNotExist_ReturnsNotFound(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
 
 func Test_GetUser_WithoutQuery_ReturnsBadRequest(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
 
 func Test_PostUser_WithNewName_ReturnsNewUserURL(t *testing.T) {
+	// TODO implement
+	t.SkipNow()
 
 	// TODO verify user at URL matches expected
 }
 
 func Test_PostUser_WithExistingName_ReturnsConflict(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
 
 func Test_PostUser_WithoutName_ReturnsBadRequest(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
 
 func Test_DeleteUser_WhenIDExists_ReturnsNotFoundOnSubsequentGet(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
 
 func Test_DeleteUser_WhenIDDoesNotExist_ReturnsNotFound(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
 
 func Test_DeleteUser_WithoutID_ReturnsBadRequest(t *testing.T) {
-
+	// TODO implement
+	t.SkipNow()
 }
--- model/user.go
+++ model/user.go
@@ -1,7 +1,34 @@
 package model
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // User contains profile info for a user account.
 type User struct {
 	ID   uint64
 	Name string
+}
+
+// UnmarshalJSON gets a User from a JSON encoding.
+func (u *User) UnmarshalJSON(data []byte) error {
+	nillable := struct {
+		ID   *uint64 `json:"id"`
+		Name *string `json:"name"`
+	}{}
+
+	err := json.Unmarshal(data, &nillable)
+
+	if err != nil {
+		return err
+	} else if nillable.ID == nil {
+		return fmt.Errorf("missing \"id\" field")
+	} else if nillable.Name == nil {
+		return fmt.Errorf("missing \"name\" field")
+	}
+
+	u.ID = *nillable.ID
+	u.Name = *nillable.Name
+	return nil
 }
